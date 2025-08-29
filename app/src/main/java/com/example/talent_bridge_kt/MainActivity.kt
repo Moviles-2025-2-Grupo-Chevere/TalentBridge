c8f8cfc2179aac18842de4624cff4e23158a5563
juanlozano3 <juanlozanog9@gmail.com>
2025-08-29 16:02:35 -0500

    proyecto de kotilin creado en android 9 minimo

---
+++ app/src/main/java/com/example/talent_bridge_kt/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.talent_bridge_kt
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.talent_bridge_kt.ui.theme.Talent_bridge_ktTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Talent_bridge_ktTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Grupo chevere del grupo chevere que hace kotlin",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Talent_bridge_ktTheme {
+        Greeting("Grupo chevere del grupo chevere que hace kotlin")
+    }
+}
