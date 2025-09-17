98770ea33b57c9365ed2bc2f275020062847d419
Danielfts <d.trivino@uniandes.edu.co>
2025-09-17 11:43:34 -0500

    Feature: scaffold prototype menu

---
+++ talent_bridge_fl/lib/views/prototype_menu.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PrototypeMenu extends StatelessWidget {
+const PrototypeMenu({ super.key });
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold();
+  }
+}
