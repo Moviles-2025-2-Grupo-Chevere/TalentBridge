e37e553829ba2916b042a7274f7cf84fef3d20f4
MarianaOrtegaRam <m.ortegar@uniandes.edu.co>
2025-09-20 01:00:25 -0500

    Feature: scaffold Signup screen with title (StatefulWidget)

--- talent_bridge_fl/lib/views/signup/signup.dart
+++ talent_bridge_fl/lib/views/signup/signup.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 
-class Signup extends StatelessWidget {
-const Signup({ super.key });
+class Signup extends StatefulWidget {
+  const Signup({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return Text('Signup view');
+  State<Signup> createState() => _SignupState();
+}
+
+class _SignupState extends State<Signup> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xFFFEF7E6), 
+      body: SafeArea(
+        child: Center(
+          child: Text(
+            'Create your account',
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.w700,
+              color: Color(0xFFFFC107),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
