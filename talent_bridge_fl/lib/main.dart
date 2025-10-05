e8a62cd4b9346c42ded51169b4fafe6b04d55c90
Danielfts <d.trivino@uniandes.edu.co>
2025-10-04 21:41:57 -0500

    Feature: Delete slop

--- talent_bridge_fl/lib/main.dart
+++ talent_bridge_fl/lib/main.dart
@@ -47,24 +47,3 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
-
-class AppGate extends StatelessWidget {
-  const AppGate({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Scaffold(
-            body: Center(child: CircularProgressIndicator()),
-          );
-        }
-
-        final user = snapshot.data;
-        return user == null ? const Login() : const HomeView();
-      },
-    );
-  }
-}
