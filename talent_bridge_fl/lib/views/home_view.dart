de78fb9acbf1eaa6ca5d947e99a6fdb942e9f98f
Danielfts <d.trivino@uniandes.edu.co>
2025-10-04 20:02:00 -0500

    Feature: completed navigation to main views

--- talent_bridge_fl/lib/views/home_view.dart
+++ talent_bridge_fl/lib/views/home_view.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:talent_bridge_fl/views/credits/credits.dart';
 import 'package:talent_bridge_fl/views/leader_feed/leader_feed.dart';
-import 'package:talent_bridge_fl/views/login/login.dart';
 import 'package:talent_bridge_fl/views/main-feed/main_feed.dart';
 import 'package:talent_bridge_fl/views/my-profile/my_profile.dart';
 import 'package:talent_bridge_fl/views/org-profile/org_profile.dart';
 import 'package:talent_bridge_fl/views/project/project_view.dart';
+import 'package:talent_bridge_fl/views/saved-projects/saved_projects.dart';
 import 'package:talent_bridge_fl/views/search/search.dart';
-import 'package:talent_bridge_fl/views/signup/signup.dart';
 import 'package:talent_bridge_fl/views/user-profile/user_profile.dart';
 
 class HomeView extends StatefulWidget {
@@ -18,9 +17,27 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  int _selectedPageIdx = 0;
+
+
+  var mainViews = [
+    MainViewItem(widget: MainFeed(), icon: Icon(Icons.home_outlined), label: "Home"),
+    MainViewItem(widget: Search(), icon: Icon(Icons.search), label: 'Search'),
+    MainViewItem(widget: SavedProjects(), icon: Icon(Icons.favorite_outline), label: 'Saved'),
+    MainViewItem(widget: MyProfile(), icon: Icon(Icons.person_outline), label: 'Profile'),
+
+  ];
+  
+  void _selectPage(int idx){
+    setState(() {
+      _selectedPageIdx = idx;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    Widget activePage = mainViews[_selectedPageIdx].widget;
+
     return Scaffold(
       appBar: AppBar(
         title: Image.asset(
@@ -38,22 +55,20 @@ class _HomeViewState extends State<HomeView> {
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
         type: BottomNavigationBarType.fixed,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite_outline),
-            label: 'Saved',
-          ),
-          BottomNavigationBarItem(icon: Icon(Icons.menu), label: 'Menu'),
-          // BottomNavigationBarItem(icon: Icon(Icons.code), label: 'Prototype'),
-        ],
+        items: mainViews.map((i) => BottomNavigationBarItem(icon: i.icon, label: i.label)).toList(),
       ),
-      body: MainFeed(),
+      body: activePage,
     );
   }
+}
+
+class MainViewItem {
+  MainViewItem({required this.widget, required this.icon, required this.label});
+
+  final Widget widget;
+  final Icon icon;
+  final String label;
+
 }
