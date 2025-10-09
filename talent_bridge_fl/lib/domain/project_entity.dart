77319c2c7092230c23244b4e936aa33afa4bc6cb
Danielfts <d.trivino@uniandes.edu.co>
2025-10-09 17:36:15 -0500

    Feature: added id attr

--- talent_bridge_fl/lib/domain/project_entity.dart
+++ talent_bridge_fl/lib/domain/project_entity.dart
@@ -1,16 +1,10 @@
 import 'package:talent_bridge_fl/domain/user_entity.dart';
+import 'package:uuid/uuid.dart';
+
+final _uuid = Uuid();
 
 class ProjectEntity {
-  ProjectEntity({
-    required this.createdAt,
-    required this.createdById,
-    this.createdBy,
-    required this.title,
-    required this.description,
-    required this.skills,
-    this.imgUrl,
-  });
-
+  final String? id;
   final DateTime createdAt;
   final String createdById;
   final UserEntity? createdBy;
@@ -19,8 +13,20 @@ class ProjectEntity {
   final List<String> skills;
   final String? imgUrl;
 
+  ProjectEntity({
+    String? id,
+    required this.createdAt,
+    required this.createdById,
+    this.createdBy,
+    required this.title,
+    required this.description,
+    required this.skills,
+    this.imgUrl,
+  }) : id = id ?? _uuid.v4().toString();
+
   Map<String, dynamic> toMap() {
     return {
+      'id': id,
       'createdAt': createdAt.toIso8601String(),
       'createdById': createdById,
       'title': title,
