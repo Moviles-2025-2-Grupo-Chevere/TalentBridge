484cb38e05e85c3266b76d15397de87fa5df40c3
Danielfts <d.trivino@uniandes.edu.co>
2025-10-20 01:41:14 -0500

    Refactor: lifted state up

--- talent_bridge_fl/lib/components/project_list.dart
+++ talent_bridge_fl/lib/components/project_list.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:talent_bridge_fl/components/project_post.dart';
 import 'package:talent_bridge_fl/components/submit_alert_db.dart';
 import 'package:talent_bridge_fl/domain/project_entity.dart';
+import 'package:talent_bridge_fl/services/db_service.dart';
 import 'package:talent_bridge_fl/services/firebase_service.dart';
 
 class ProjectList extends StatefulWidget {
@@ -15,6 +16,8 @@ class ProjectList extends StatefulWidget {
 
 class _ProjectListState extends State<ProjectList> {
   final firebaseService = FirebaseService();
+  final dbService = const DbService();
+
   late final List<ProjectEntity> projects;
 
   void submitProjectApplication(
@@ -54,6 +57,101 @@ class _ProjectListState extends State<ProjectList> {
     });
   }
 
+  Future<void> onSaveProject(ProjectEntity project) async {
+    var scaffoldMessenger = ScaffoldMessenger.of(context);
+    try {
+      await dbService.insertSavedProject(project);
+      if (context.mounted) {
+        scaffoldMessenger.clearSnackBars();
+        scaffoldMessenger.showSnackBar(
+          SnackBar(content: Text('Project saved as favorite')),
+        );
+      }
+    } catch (e) {
+      if (context.mounted) {
+        scaffoldMessenger.clearSnackBars();
+        scaffoldMessenger.showSnackBar(
+          SnackBar(content: Text('Error saving project to favorites')),
+        );
+      }
+    }
+  }
+
+  Future<void> onRemoveProject(ProjectEntity project) async {
+    var scaffoldMessenger = ScaffoldMessenger.of(context);
+    try {
+      await dbService.removeSavedProject(project.id!);
+      removeFromList(project);
+      if (context.mounted) {
+        scaffoldMessenger.clearSnackBars();
+        scaffoldMessenger.showSnackBar(
+          SnackBar(content: Text('Project removed from favorites')),
+        );
+      }
+    } catch (e) {
+      if (context.mounted) {
+        scaffoldMessenger.clearSnackBars();
+        scaffoldMessenger.showSnackBar(
+          SnackBar(content: Text('Error removing project from favorites')),
+        );
+      }
+    }
+  }
+
+  void showSaveModal(ProjectEntity p) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("Save project"),
+        content: Text("Save this project in your favorites?"),
+        actions: [
+          OutlinedButton.icon(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            label: Text("Cancel"),
+            icon: Icon(Icons.cancel),
+          ),
+          FilledButton.icon(
+            onPressed: () {
+              onSaveProject(p);
+              Navigator.of(context).pop();
+            },
+            label: Text("Save"),
+            icon: Icon(Icons.save),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void showRemoveModal(ProjectEntity p) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("Remove project"),
+        content: Text("Remove this project from your favorites?"),
+        actions: [
+          OutlinedButton.icon(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            label: Text("Cancel"),
+            icon: Icon(Icons.cancel),
+          ),
+          FilledButton.icon(
+            onPressed: () {
+              onRemoveProject(p);
+              Navigator.of(context).pop();
+            },
+            label: Text("Remove"),
+            icon: Icon(Icons.save),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   void initState() {
     super.initState();
@@ -69,7 +167,8 @@ class _ProjectListState extends State<ProjectList> {
         onDismissed: (direction) {},
         child: ProjectPost(
           project: projects[index],
-          removeFromList: removeFromList,
+          showRemoveModal: showRemoveModal,
+          showSaveModal: showSaveModal,
           showApplyModal: (String userId, String projectId) {
             showDialog(
               context: context,
--- talent_bridge_fl/lib/components/project_post.dart
+++ talent_bridge_fl/lib/components/project_post.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:talent_bridge_fl/domain/project_entity.dart';
-import 'package:talent_bridge_fl/services/db_service.dart';
 import 'package:talent_bridge_fl/services/firebase_service.dart';
 import 'package:talent_bridge_fl/views/user-profile/user_profile.dart';
 
@@ -9,109 +8,14 @@ class ProjectPost extends StatelessWidget {
     super.key,
     required this.project,
     required this.showApplyModal,
-    required this.removeFromList,
+    required this.showSaveModal,
+    required this.showRemoveModal,
   });
 
   final ProjectEntity project;
   final void Function(String, String) showApplyModal;
-  final void Function(ProjectEntity) removeFromList;
-  final dbService = const DbService();
-
-  Future<void> onSaveProject(BuildContext context) async {
-    var scaffoldMessenger = ScaffoldMessenger.of(context);
-    try {
-      await dbService.insertSavedProject(project);
-      if (context.mounted) {
-        scaffoldMessenger.clearSnackBars();
-        scaffoldMessenger.showSnackBar(
-          SnackBar(content: Text('Project saved as favorite')),
-        );
-      }
-    } catch (e) {
-      if (context.mounted) {
-        scaffoldMessenger.clearSnackBars();
-        scaffoldMessenger.showSnackBar(
-          SnackBar(content: Text('Error saving project to favorites')),
-        );
-      }
-    }
-  }
-
-  Future<void> onRemoveProject(BuildContext context) async {
-    var scaffoldMessenger = ScaffoldMessenger.of(context);
-    try {
-      await dbService.removeSavedProject(project.id!);
-      removeFromList(project);
-      if (context.mounted) {
-        scaffoldMessenger.clearSnackBars();
-        scaffoldMessenger.showSnackBar(
-          SnackBar(content: Text('Project removed from favorites')),
-        );
-      }
-    } catch (e) {
-      if (context.mounted) {
-        scaffoldMessenger.clearSnackBars();
-        scaffoldMessenger.showSnackBar(
-          SnackBar(content: Text('Error removing project from favorites')),
-        );
-      }
-    }
-  }
-
-  void showSaveModal(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text("Save project"),
-        content: Text("Save this project in your favorites?"),
-        actions: [
-          OutlinedButton.icon(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            label: Text("Cancel"),
-            icon: Icon(Icons.cancel),
-          ),
-          FilledButton.icon(
-            onPressed: () {
-              onSaveProject(context);
-              Navigator.of(context).pop();
-            },
-            label: Text("Save"),
-            icon: Icon(Icons.save),
-          ),
-        ],
-      ),
-    );
-  }
-
-  void showRemoveModal(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text("Remove project"),
-        content: Text("Remove this project from your favorites?"),
-        actions: [
-          OutlinedButton.icon(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            label: Text("Cancel"),
-            icon: Icon(Icons.cancel),
-          ),
-          FilledButton.icon(
-            onPressed: () {
-              onRemoveProject(context);
-              Navigator.of(context).pop();
-            },
-            label: Text("Remove"),
-            icon: Icon(Icons.save),
-          ),
-        ],
-      ),
-    );
-  }
-
+  final void Function(ProjectEntity) showSaveModal;
+  final void Function(ProjectEntity) showRemoveModal;
   @override
   Widget build(BuildContext context) {
     final firebaseService = FirebaseService();
@@ -180,12 +84,12 @@ class ProjectPost extends StatelessWidget {
                 TextButton(onPressed: () {}, child: Text('Comments')),
                 if (!project.isFavorite)
                   TextButton(
-                    onPressed: () => showSaveModal(context),
+                    onPressed: () => showSaveModal(project),
                     child: Text('Save'),
                   ),
                 if (project.isFavorite)
                   TextButton(
-                    onPressed: () => showRemoveModal(context),
+                    onPressed: () => showRemoveModal(project),
                     child: Text('Remove'),
                   ),
                 TextButton(
