039ee2cb5d55f5e414c4f7c67d1fea9bc22d7c20
David Fuquen <d.fuquen@uniandes.edu.co>
2025-10-20 06:40:19 -0500

    Feature: Fully working select and upload of CVs pdf

--- talent_bridge_fl/lib/main.dart
+++ talent_bridge_fl/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'views/login/login.dart';
 import 'services/connectivity_service.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:file_picker/file_picker.dart';
 
 // Global navigator key
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
