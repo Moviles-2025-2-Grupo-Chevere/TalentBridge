e802ce1863e746f464a44142d9a172af7dc2e3ea
Danielfts <d.trivino@uniandes.edu.co>
2025-10-22 11:19:27 -0500

    Feature: save image locally with uid

--- talent_bridge_fl/lib/services/profile_pic_storage.dart
+++ talent_bridge_fl/lib/services/profile_pic_storage.dart
@@ -2,14 +2,19 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:talent_bridge_fl/services/firebase_service.dart';
 
 class ProfileStorage {
-  static String? _profileImagePath;
+  static final _fbService = FirebaseService();
 
   /// Get the current profile image path
   static Future<String?> getLocalProfileImagePath() async {
+    final uid = _fbService.currentUid();
+    if (uid == null) {
+      throw Exception("Uid not found before saving image");
+    }
     final directory = await getApplicationDocumentsDirectory();
-    final appPath = '${directory.path}/profile_picture.jpg';
+    final appPath = '${directory.path}/$uid.jpg';
     final file = File(appPath);
     if (file.existsSync()) {
       return appPath;
@@ -19,9 +24,13 @@ class ProfileStorage {
 
   /// Save the profile image path
   static Future<String> saveProfilePictureLocally(String tempPath) async {
+    final uid = _fbService.currentUid();
+    if (uid == null) {
+      throw Exception("Uid not found before saving image");
+    }
     final localImage = File(tempPath);
     final directory = await getApplicationDocumentsDirectory();
-    final appPath = '${directory.path}/profile_picture.jpg';
+    final appPath = '${directory.path}/$uid.jpg';
     final newFile = await localImage.copy(appPath);
     debugPrint('Profile image path saved: $tempPath');
     return newFile.path;
@@ -29,7 +38,6 @@ class ProfileStorage {
 
   /// Clear the profile image path
   static void clearProfileImagePath() {
-    _profileImagePath = null;
     // In a real app, you would remove this from storage here
     debugPrint('Profile image path cleared');
   }
