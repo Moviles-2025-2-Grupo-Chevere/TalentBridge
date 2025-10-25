7871bf2d4c188a46f6dd663da45d0f64164c75f5
Danielfts <d.trivino@uniandes.edu.co>
2025-10-25 00:12:41 -0500

    Feature: completed basic notification reactions

--- talent_bridge_fl/lib/main.dart
+++ talent_bridge_fl/lib/main.dart
@@ -1,16 +1,20 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:talent_bridge_fl/views/splash_screen.dart';
 import 'services/connectivity_service.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'firebase_options.dart';
 
 // Global navigator key
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // await deleteDatabase(join(await getDatabasesPath(), 'talent_bridge.db'));
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(ProviderScope(child: const TalentBridge()));
 }
 
@@ -23,12 +27,25 @@ class TalentBridge extends StatefulWidget {
 class _TalentBridgeState extends State<TalentBridge> {
   final ConnectivityService _connectivityService = ConnectivityService();
 
+  Future<void> _setupNotifications() async {
+    var messaging = FirebaseMessaging.instance;
+    final notificationSettings = await messaging.requestPermission(
+      alert: true,
+      badge: true,
+      sound: true,
+      provisional: true,
+    );
+    final token = await messaging.getToken();
+    print("Token: $token");
+  }
+
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) {
       _connectivityService.initialize(this.context);
     });
+    _setupNotifications();
   }
 
   @override
