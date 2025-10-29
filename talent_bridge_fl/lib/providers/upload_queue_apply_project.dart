15e1ebe7d1ed65ea9468ee2ac95289ca23b61638
David Fuquen <d.fuquen@uniandes.edu.co>
2025-10-29 16:46:13 -0500

    Created new upload queue

---
+++ talent_bridge_fl/lib/providers/upload_queue_apply_project.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:talent_bridge_fl/services/firebase_service.dart';
+
+final projectApplyUploadProvider = NotifierProvider(
+  ProjectApplyUploadNotifier.new,
+);
+
+class ProjectApplyUploadNotifier extends Notifier<String?> {
+  final _connectivity = Connectivity();
+  final _fbService = FirebaseService();
+  StreamSubscription<List<ConnectivityResult>>? _subscription;
+
+  /// Uses stream concurrency to listen to connectivity changes
+  @override
+  String? build() {
+    state = null;
+
+    _subscription = _connectivity.onConnectivityChanged.listen((status) {
+      if (status[0] != ConnectivityResult.none) {
+        if (state != null) {
+          tryUpload(state!);
+        }
+      }
+    });
+
+    ref.onDispose(() => _subscription?.cancel());
+    return state;
+  }
+
+  Future<TaskSnapshot?> enqueuePfpUpload(String path) async {
+    state = path;
+    return await tryUpload(path);
+  }
+
+  Future<TaskSnapshot?> tryUpload(String path) async {
+    final file = File(path);
+    final uploadResult = await _fbService.uploadPFP(file);
+    if (uploadResult != null) {
+      state = null;
+    }
+    return uploadResult;
+  }
+}
