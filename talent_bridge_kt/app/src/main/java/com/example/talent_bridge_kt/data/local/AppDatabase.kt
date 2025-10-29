c7481d9238a30654211347220b86cbe149b87fd9
juanlozano3 <juanlozanog9@gmail.com>
2025-10-28 21:44:43 -0500

    feature: eventaul conectivity for cahce projects

--- talent_bridge_kt/app/src/main/java/com/example/talent_bridge_kt/data/local/AppDatabase.kt
+++ talent_bridge_kt/app/src/main/java/com/example/talent_bridge_kt/data/local/AppDatabase.kt
@@ -2,10 +2,13 @@ package com.example.talent_bridge_kt.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.talent_bridge_kt.data.local.dao.FeedProjectDao
 import com.example.talent_bridge_kt.data.local.dao.ProjectDao
+import com.example.talent_bridge_kt.data.local.entities.FeedProjectEntity
 import com.example.talent_bridge_kt.data.local.entities.ProjectEntity
 
-@Database(entities = [ProjectEntity::class], version = 2)
+@Database(entities = [ProjectEntity::class, FeedProjectEntity::class], version = 3)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun projectDao(): ProjectDao
+    abstract fun feedProjectDao(): FeedProjectDao
 }
