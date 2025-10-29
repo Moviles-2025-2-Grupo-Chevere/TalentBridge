caab66a76263a7a43183dce1f5b3e170ca6aafda
Danielfts <d.trivino@uniandes.edu.co>
2025-10-28 21:34:00 -0500

    Feature: Added skill seeding tooling

--- tools/seed.js
+++ tools/seed.js
@@ -2,14 +2,16 @@ const admin = require("firebase-admin");
 const data = require("./data.json"); // Your data file
 
 admin.initializeApp({
-  credential: admin.credential.cert(require("./serviceAccountKey.json")),
+  credential: admin.credential.cert(
+    require("./secrets/talentbridge-dev-21980-firebase-adminsdk-fbsvc-c59216b4a5.json")
+  ),
 });
 
 const db = admin.firestore();
 
 async function seed() {
   const batchSize = 500;
-  const collectionName = "myCollection";
+  const collectionName = "skills";
   const collectionRef = db.collection(collectionName);
 
   let batch = db.batch();
