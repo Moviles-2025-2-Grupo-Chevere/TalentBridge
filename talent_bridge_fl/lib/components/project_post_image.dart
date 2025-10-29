246d3e89e3d5ec31426d2e1305cb9a2c14cc9344
Danielfts <d.trivino@uniandes.edu.co>
2025-10-29 12:23:56 -0500

    Fix: bug in cached images

--- talent_bridge_fl/lib/components/project_post_image.dart
+++ talent_bridge_fl/lib/components/project_post_image.dart
@@ -1,43 +1,81 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:talent_bridge_fl/domain/project_entity.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 /// Uses Cached network image to display the image of a project
-class ProjectPostImage extends StatelessWidget {
+class ProjectPostImage extends StatefulWidget {
   const ProjectPostImage({
     super.key,
-    required this.imageUrlFuture,
-    required this.imageKey,
+    required this.project,
   });
 
-  final Future<String?>? imageUrlFuture;
-  final String imageKey;
+  final ProjectEntity project;
+
+  @override
+  State<ProjectPostImage> createState() => _ProjectPostImageState();
+}
+
+class _ProjectPostImageState extends State<ProjectPostImage> {
+  String? imageUrl;
+  late String imgKey;
+
+  Future setImageUrl() async {
+    final shared = await SharedPreferences.getInstance();
+    final storage = FirebaseStorage.instance;
+    final localUrl = shared.getString(imgKey);
+    if (mounted) {
+      setState(() {
+        imageUrl = localUrl;
+      });
+    }
+    try {
+      final remoteUrl = await storage
+          .ref()
+          .child('project_images/$imgKey')
+          .getDownloadURL()
+          .then(
+            (value) {
+              shared.setString(imgKey, value);
+              return value;
+            },
+          )
+          .timeout(Duration(seconds: 10));
+      if (mounted) {
+        setState(() {
+          imageUrl = remoteUrl;
+        });
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    imgKey = widget.project.id!;
+    setImageUrl();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: imageUrlFuture,
-      builder: (context, snapshot) {
-        if (!snapshot.hasError) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 16,
-              vertical: 8,
-            ),
-            child: ConstrainedBox(
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: imageUrl == null
+          ? SizedBox.shrink()
+          : ConstrainedBox(
               constraints: BoxConstraints(maxHeight: 300),
               child: CachedNetworkImage(
-                cacheKey: imageKey,
-                imageUrl: snapshot.data ?? '',
+                imageUrl: imageUrl!,
+                cacheKey: imgKey,
+                errorWidget: (context, url, error) => Center(
+                  child: Icon(Icons.error),
+                ),
                 placeholder: (context, url) => CircularProgressIndicator(),
-                errorWidget: (context, url, error) => const SizedBox.shrink(),
-                fit: BoxFit.contain,
               ),
             ),
-          );
-        }
-
-        return const SizedBox.shrink();
-      },
     );
   }
 }
