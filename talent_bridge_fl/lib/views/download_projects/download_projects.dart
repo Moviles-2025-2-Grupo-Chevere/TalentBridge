51bb6603fbe2c38d3c7d5aa3b6417a000c7cfbda
Danielfts <d.trivino@uniandes.edu.co>
2025-10-30 12:22:36 -0500

    Add project list

--- talent_bridge_fl/lib/views/download_projects/download_projects.dart
+++ talent_bridge_fl/lib/views/download_projects/download_projects.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:talent_bridge_fl/providers/profile_provider.dart';
+import 'package:talent_bridge_fl/util/string_utils.dart';
 
-class DownloadProjects extends StatelessWidget {
+class DownloadProjects extends ConsumerWidget {
   const DownloadProjects({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final user = ref.watch(profileProvider);
+    return user == null
+        ? Center(child: CircularProgressIndicator())
+        : Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListView(
+              children: [
+                ...user.projects!.map(
+                  (e) => ListTile(
+                    title: Text(capitalize(e.title)),
+                    onTap: () {},
+                  ),
+                ),
+              ],
+            ),
+          );
   }
 }
