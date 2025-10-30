e9cc36d6efeada04af8e3de56a0c510118e06e31
Danielfts <d.trivino@uniandes.edu.co>
2025-10-30 12:02:42 -0500

    Feature: Added scaffold of downloads view

---
+++ talent_bridge_fl/lib/views/download_projects/download_projects.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DownloadProjects extends StatelessWidget {
+  const DownloadProjects({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
