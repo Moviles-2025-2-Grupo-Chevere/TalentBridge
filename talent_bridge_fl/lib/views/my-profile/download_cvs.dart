3b5540ea310484f320c9d620aa38713209f1b60f
David Fuquen <d.fuquen@uniandes.edu.co>
2025-11-03 21:35:25 -0500

    Feature: Created needed file and added imports

---
+++ talent_bridge_fl/lib/views/my-profile/download_cvs.dart
@@ -0,0 +1,6 @@
+import 'dart:typed_data';
+import 'package:archive/archive_io.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
