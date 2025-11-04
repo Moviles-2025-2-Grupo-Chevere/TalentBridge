bc3f23fe3d3737e3f08c9620b219dffc7f8de28c
David Fuquen <d.fuquen@uniandes.edu.co>
2025-11-03 21:39:51 -0500

    Feature: Methods for downloading files from urls

--- talent_bridge_fl/lib/views/my-profile/download_cvs.dart
+++ talent_bridge_fl/lib/views/my-profile/download_cvs.dart
@@ -4,3 +4,26 @@ import 'package:file_picker/file_picker.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+
+class DownloadCVs {
+  // Download a single file from firebase url
+  static Future<Uint8List> _downloadFile(String url) async {
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      return response.bodyBytes;
+    } else {
+      throw Exception('Failed to download file from $url');
+    }
+  }
+
+  // Download multiple CV files given their URLs
+  static Future<List<Uint8List>> _downloadCVFiles(List<String> cvUrls) async {
+    final List<Future<Uint8List>> downloadFutures = [];
+
+    for (final url in cvUrls) {
+      downloadFutures.add(_downloadFile(url));
+    }
+
+    return await Future.wait(downloadFutures);
+  }
+}
