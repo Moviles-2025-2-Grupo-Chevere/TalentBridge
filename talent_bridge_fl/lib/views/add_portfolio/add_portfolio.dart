d451fb4c25a235dbd11b811f21fe91d6b9db949b
David-Fuq <d.fuquen@uniandes.edu.co>
2025-11-29 01:53:36 -0500

    feature: scaffold view

---
+++ talent_bridge_fl/lib/views/add_portfolio/add_portfolio.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddPortfolio extends StatelessWidget {
+  const AddPortfolio({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
